c85c598c6b76fa627c7624d8a7e1f7527a725465
Darya <daryakuzmich0@gmail.com>
2023-01-30 12:50:15 +0300

    Создание метода по поиску элементов согласно условию задачи

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -2,3 +2,17 @@
 string str = Console.ReadLine()!;
 string[] array1 = str.Split(' ');
 string[] array2 = new string[array1.Length];
+
+
+void FindItems(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
